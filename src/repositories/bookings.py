8021b3fae953b28736be1bfe3dea2e8fd494e873
exl <levzavadskiy999@gmail.com>
2025-06-26 23:26:33 +0300

    Задание 24

--- src/repositories/bookings.py
+++ src/repositories/bookings.py
@@ -9,7 +9,7 @@ from src.repositories.base import BaseRepository
 from src.models.bookings import BookingsOrm
 from src.repositories.mappers.mappers import BookingDataMapper
 from src.repositories.utils import rooms_ids_for_booking
-from src.schemas.bookings import Booking
+from src.schemas.bookings import Booking, BookingAdd
 
 
 class BookingsRepository(BaseRepository):
@@ -24,15 +24,38 @@ class BookingsRepository(BaseRepository):
         res = await self.session.execute(query)
         return [self.mapper.map_to_domain_entity(booking) for booking in res.scalars().all()]
 
-    async def add_booking(self, date_from, date_to, hotel_id, room_id):
-        rooms_ids_to_get = rooms_ids_for_booking(date_from, date_to, hotel_id)
-        result = await self.session.execute(rooms_ids_to_get)
-        rooms_ids_to_get = []
-        for row in result.fetchall():
-            rooms_ids_to_get.append(row[0])
+    # async def add_booking(self, date_from, date_to, hotel_id, room_id):
+    #     rooms_ids_to_get = rooms_ids_for_booking(date_from, date_to, hotel_id)
+    #     result = await self.session.execute(rooms_ids_to_get)
+    #     rooms_ids_to_get = []
+    #     for row in result.fetchall():
+    #         rooms_ids_to_get.append(row[0])
+    #
+    #     if room_id not in rooms_ids_to_get:
+    #         raise HTTPException(status_code=409, detail="Больше таких номеров нету")
 
-        if room_id not in rooms_ids_to_get:
-            raise HTTPException(status_code=409, detail="Больше таких номеров нету")
+    async def add_booking(self, data: BookingAdd, hotel_id: int):
+        #получаем свободные номера(айди номеров) по датам
+        rooms_ids_to_get = rooms_ids_for_booking(
+            date_from=data.date_from,
+            date_to=data.date_to,
+            hotel_id=hotel_id,
+        )
+        #выполняем запрос к базе
+        rooms_ids_to_book_res = await self.session.execute(rooms_ids_to_get)
+        #получаем данные(айди номеров) в виде списка, scalars - первая столбик, all - все айдишники
+        rooms_ids_to_book: list[int] = rooms_ids_to_book_res.scalars().all()
+        #проверка есть ли room_id в доступных номерах
+        if data.room_id in rooms_ids_to_book:
+            #если есть создаём бронирование
+            new_booking = await self.add(data)
+            #возвращаем бронь
+            return new_booking
+        else:
+            #иначе ошибка
+            raise HTTPException(500)
+
+
 
 
 
--- tests/integration_tests/bookings/test_api.py
+++ tests/integration_tests/bookings/test_api.py
@@ -1,15 +1,65 @@
-async def test_add_booking(db, authenticated_ac):
+import pytest
+from sqlalchemy import delete
+
+from src.models.bookings import BookingsOrm
+
+
+@pytest.mark.parametrize("room_id, date_from, date_to, status_code", [
+    (1, "2024-08-01", "2024-08-10", 200),
+    (2, "2024-08-01", "2024-08-10", 200),
+    (3, "2024-08-01", "2024-08-10", 500),
+])
+async def test_add_booking(
+        room_id, date_from, date_to, status_code,
+        db, authenticated_ac
+):
     room_id = (await db.rooms.get_all())[0].id
     response = await authenticated_ac.post(
         "/bookings",
         json={
             "room_id": room_id,
-            "date_from": "2024-08-01",
-            "date_to": "2024-08-10",
+            "date_from": date_from,
+            "date_to": date_to,
         }
     )
-    assert response.status_code == 200
-    res = response.json()
-    assert isinstance(res, dict)
-    assert res["status"] == "OK"
-    assert "data" in res
+    assert response.status_code == status_code
+    if status_code == 200:
+        res = response.json()
+        assert isinstance(res, dict)
+        assert res["status"] == "OK"
+        assert "data" in res
+
+@pytest.fixture(scope="session")
+async def delete_all_bookings(db):
+    await db.session.execute(delete(BookingsOrm))
+    await db.commit()
+
+@pytest.mark.parametrize("room_id, date_from, date_to, expected_status", [
+    (1, "2024-08-01", "2024-08-10", 200),
+    (2, "2025-08-01", "2025-08-10", 200),
+    (1, "2024-08-01", "2024-08-10", 500),
+])
+async def test_add_and_get_bookings(
+    room_id, date_from, date_to, expected_status,
+    db, authenticated_ac
+):
+    response = await authenticated_ac.post(
+        "/bookings",
+        json={
+            "room_id": room_id,
+            "date_from": date_from,
+            "date_to": date_to,
+        }
+    )
+    assert response.status_code == expected_status
+    if expected_status == 200:
+        res = response.json()
+        assert isinstance(res, dict)
+        assert res["status"] == "OK"
+        assert "data" in res
+
+    response_me = await authenticated_ac.get("/bookings/me")
+    assert response_me.status_code == 200
+    bookings = response_me.json()
+    assert isinstance(bookings, list)
+    assert len(bookings) >= 1
